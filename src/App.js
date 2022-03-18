fb9c4690be3c8dcb01201866ff82c476d967ba54
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-18 11:12:43 +0100

    have flashcards in state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 
+const url = 'http://localhost:5000/flashcards';
+
 function App() {
-  return (
-    <div className="App">
-		<p>Hello</p>
-    </div>
-  );
+	const [flashcards, setFlashcards] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(url);
+			const flashcards = await response.json();
+			setFlashcards(flashcards);
+		})();	
+	}, []);
+
+	return (
+		<div className="App">
+		<h1>Flashcards</h1>
+			<div>We have {flashcards.length} flashcards.</div>
+		</div>
+	);
 }
 
 export default App;
